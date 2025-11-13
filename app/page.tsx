fccc20ef4bbe412276dc0d82e33033df4e921e41
Nate Babyak <nate.babyak@outlook.com>
2025-11-13 10:42:47 -0500

    updated home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,45 @@
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
-import { Button } from "@/components/ui/button";
-import { ButtonGroup } from "@/components/ui/button-group";
+import { ModifierKbd } from "@/components/modifier-kbd";
 import {
   InputGroup,
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group";
-import { ArrowRight, Search } from "lucide-react";
+import { Kbd, KbdGroup } from "@/components/ui/kbd";
+import { Separator } from "@/components/ui/separator";
+import { Search } from "lucide-react";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="text-center">
-        <h1 className="text-7xl font-bold">LaTeXdex</h1>
-        <p className="text-muted-foreground">
-          The free and open-source LaTeX database
-        </p>
-        <ButtonGroup>
-          <ButtonGroup>
-            <InputGroup>
-              <InputGroupAddon>
+      <main>
+        <article>
+          <div className="grid gap-2 text-center">
+            <h1 className="text-7xl font-bold">LaTeXdex</h1>
+            <p className="text-muted-foreground text-lg">
+              The free and open-source LaTeX database
+            </p>
+          </div>
+          <div className="mx-auto grid gap-2">
+            <InputGroup className="mx-auto w-full max-w-sm">
+              <InputGroupAddon align="inline-start">
                 <Search />
               </InputGroupAddon>
               <InputGroupInput placeholder="Search..." />
+              <InputGroupAddon align="inline-end">
+                <KbdGroup>
+                  <ModifierKbd />
+                  <Kbd>K</Kbd>
+                </KbdGroup>
+              </InputGroupAddon>
             </InputGroup>
-          </ButtonGroup>
-          <ButtonGroup>
-            <Button size="icon" variant="outline">
-              <ArrowRight />
-            </Button>
-          </ButtonGroup>
-        </ButtonGroup>
+          </div>
+          <Separator />
+
+          <Separator />
+        </article>
       </main>
       <Footer />
     </>
---
+++ components/new-button.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import { useState } from "react";
+
+export function NewButton() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger>
+        <Button>
+          <PlusCircle />
+          Create New
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+}
