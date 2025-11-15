46d0338e998290846466d9fc9eef99d5c6e58873
Nate Babyak <nate.babyak@outlook.com>
2025-11-14 23:21:59 -0500

    added Empty component

---
+++ app/search/page.tsx
@@ -0,0 +1,54 @@
+import { ArrowUpRightIcon, FileExclamationPoint, Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Empty,
+  EmptyContent,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { Header } from "@/components/layout/header";
+import { Footer } from "@/components/layout/footer";
+import prisma from "@/lib/prisma";
+import { SearchInput } from "./search-input";
+
+export default async function Page() {
+  const allEntries = await prisma.entry.findMany();
+
+  return (
+    <>
+      <Header />
+      <SearchInput />
+      <Empty>
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <FileExclamationPoint />
+          </EmptyMedia>
+          <EmptyTitle>No Entries Found</EmptyTitle>
+          <EmptyDescription>
+            You haven&apos;t created any projects yet. Get started by creating
+            your first project.
+          </EmptyDescription>
+        </EmptyHeader>
+        <EmptyContent>
+          <div className="flex gap-2">
+            <Button>Create Project</Button>
+            <Button variant="outline">Import Project</Button>
+          </div>
+        </EmptyContent>
+        <Button
+          variant="link"
+          asChild
+          className="text-muted-foreground"
+          size="sm"
+        >
+          <a href="#">
+            Learn More <ArrowUpRightIcon />
+          </a>
+        </Button>
+      </Empty>
+      <Footer />
+    </>
+  );
+}
