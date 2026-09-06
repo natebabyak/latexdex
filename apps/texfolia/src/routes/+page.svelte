b14fe1d215ae052667d5be5b87d188b1946c95b1
Nate Babyak <nate.babyak@outlook.com>
2026-09-06 08:07:00 -0400

    feat: init tauri monorepo

---
+++ apps/texfolia/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import * as Resizable from "$lib/components/ui/resizable/index";
+  import { Terminal } from "@xterm/xterm";
+
+  const term = new Terminal();
+</script>
+
+<header>
+    <Button
+</header>
+<main class="size-screen">
+  <Resizable.PaneGroup direction="vertical">
+    <Resizable.Pane>
+      <div>Hello</div>
+    </Resizable.Pane>
+    <Resizable.Handle />
+    <Resizable.Pane>dawdwa</Resizable.Pane>
+  </Resizable.PaneGroup>
+</main>
+<footer>
+    
+</footer>
